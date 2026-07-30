749f7d23c0c215b44aa1f0b9ab597abdd6c725dd
Thom Johansen <thomj@pvv.org>
2026-07-30 19:52:10 +0200

    Remove body, we want these to be abstract.

--- synth.ts
+++ synth.ts
@@ -28,30 +28,21 @@ namespace orchestra {
     //% blockId=orch_set_parameter
     //% block="set preset %preset parameter %param to %val"
     //% shim=orchestra::setParameter
-    export function setParameter(preset: SynthPreset, param: SynthParameter, val: number): void
-    {
-        return
-    }
+    export declare function setParameter(preset: SynthPreset, param: SynthParameter, val: number): void;
 
     //% help=orch/get-synth-parameter weight=30
     //% group="Orchestra"
     //% blockId=orch_get_parameter
     //% block="get value of preset %preset parameter %param"
     //% shim=orchestra::getParameter
-    export function getParameter(preset: SynthPreset, param: SynthParameter): number
-    {
-        return 0
-    }
+    export declare function getParameter(preset: SynthPreset, param: SynthParameter): number;
 
     //% help=orch/play-sample weight=30
     //% group="Orchestra"
     //% blockId=orch_play_sample
     //% block="play sample %sample with gain %gain"
     //% shim=orchestra::playSample
-    export function playSample(sample: Sample, gain: number): void
-    {
-        return
-    }
+    export declare function playSample(sample: Sample, gain: number);
 
     //% help=orch/note weight=30
     //% group="Orchestra"
@@ -62,10 +53,7 @@ namespace orchestra {
     //% velocity.min=0 velocity.max=127
     //% note.defl=69 duration.defl=1000 velocity.defl=127 preset.defl=1 s
     //% shim=orchestra::note
-    export function note(note?: number, duration?: number, velocity?: number, preset?: SynthPreset): void
-    {
-        return
-    }
+    export declare function note(note?: number, duration?: number, velocity?: number, preset?: SynthPreset): void;
 
     //% help=orch/note-off weight=30
     //% group="Orchestra"
@@ -74,9 +62,5 @@ namespace orchestra {
     //% note.min=0 note.max=127
     //% note.defl=69
     //% shim=orchestra::noteOff
-    export function noteOff(note?: number): void
-    {
-        return
-    }
-
+    export declare function noteOff(note?: number): void;
 }
